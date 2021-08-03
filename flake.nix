35e5b56837cf67e266800e668bd650504c9079e0
Justin Bedo <cu@cua0.org>
2021-08-03 14:30:37 +1000

    update and patch "cross-fingers" section to not use ram

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   inputs.nix.url = "github:nixos/nix";
-  inputs.nixpkgs.url = "nixpkgs/nixos-20.09-small";
+  inputs.nixpkgs.url = "nixpkgs/nixos-21.05-small";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, nix, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system: {
       defaultPackage = let
         pkgs = import nixpkgs { inherit system; };
         patchedNix = nix.packages."${system}".nix-static.overrideAttrs (_: {
-          patches = [ ./ca-cert-path.patch ./experimental-features.patch ./relocatable.patch ./no-fallocate.patch ./ignxattr.patch ./no-wal.patch ];
+          patches = [ ./ca-cert-path.patch ./relocatable.patch ./no-fallocate.patch ./ignxattr.patch ./cross-fingers.patch ];
           doCheck = false;
         });
       in pkgs.runCommand "build-package" {} ''
