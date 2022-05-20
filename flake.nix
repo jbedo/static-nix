033fe07e9b72c73b738524d70738047b1ecbff5b
Justin Bedo <cu@cua0.org>
2022-05-20 14:21:36 +1000

    add pruning script

--- flake.nix
+++ flake.nix
@@ -6,175 +6,196 @@
     url = "github:solidsnack/arx";
     flake = false;
   };
-  outputs = { self, nix, nixpkgs, flake-utils, arxsrc, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        patchedNix = nix.packages."${system}".nix-static.overrideAttrs (_: {
-          patches = [ ./ca-cert-path.patch ./relocatable.patch ./cross-fingers.patch ./fixed-output.patch ./registration-time.patch ];
-          doCheck = false;
-        });
-      in
-      {
-        packages =
-          let
-            # Location of srun & sallocate binaries on the HPC system
-            SLURMPREFIX = "/usr/local/slurm/current/bin";
-
-            # Location of nix store
-            # Must be shared with build nodes
-            STOREROOT = "/vast/scratch/users/$USER";
-
-            # Location of temporary files (e.g., build directories)
-            # Must be shared with build nodes
-            TMPDIR = "${STOREROOT}/tmp";
-
-            # Use proot instead of bubblewrap
-            useProot = false;
-
-            SRUN = "${SLURMPREFIX}/srun";
-            SALLOC = "${SLURMPREFIX}/salloc";
-
-            makeWrapper = name: minimal: script: pkgs.writeScript name ''
-              #!/bin/sh
-              set -e
-              set -o pipefail
-
-              function dirof {
-                SRC="$1"
-                BASE=''${SRC##*/}
-                DIR=''${SRC%"$BASE"}
-              }
-              dirof "$0"
-              SCRIPT_DIR="$( cd -- "$DIR" &> /dev/null && pwd -P)"
-              ${pkgs.lib.optionalString (!minimal) ''
-                dirof "$SCRIPT_DIR"
-                LIBEXEC="''${DIR}libexec/nix"
-                export TMPDIR="${TMPDIR}"
-                export NIX_STOREROOT="${STOREROOT}"
-                export NIX_SRUN="${SRUN}"
-                export NIX_SALLOC="${SALLOC}"
-                command -v mkdir &> /dev/null && mkdir -p "$TMPDIR"
-                command -v mkdir &> /dev/null && mkdir -p "$NIX_STOREROOT"/nix
-              ''}
-
-              # NSS wrapper remapping
-              if [ ! -z ''${USER+x} ] ; then
-                if [ ! -e ${TMPDIR}/passwd ] || [ ! -e ${TMPDIR}/group ] ; then
-                  cat /etc/passwd > ${TMPDIR}/passwd
-                  echo "$(id -un)::$(id -u):$(id -g)::$HOME:" >> ${TMPDIR}/passwd
-                  cat /etc/group > ${TMPDIR}/group
-                  echo "$(id -gn)::$(id -g):"  >> ${TMPDIR}/group
-                fi
-              fi
-
-              ${script}
-            '';
-
-            slurmNix = patchedNix.overrideAttrs (attrs: {
-              patches = attrs.patches ++ [ ./slurm-submit.patch ];
-            });
-
-            arx' = pkgs.haskellPackages.arx.overrideAttrs
-              (_: {
-                version = "git";
-                src = arxsrc;
-                preConfigure = ''
-                  echo "git" > version
-                '';
-                patches = [ ./arx-cwd.patch ];
-              });
-
-            chroot-wrapper = makeWrapper "chroot-wrapper" false ''
-              exec $LIBEXEC/nix-user-chroot "$NIX_STOREROOT/nix" "$@"
-            '';
-
-            ssh-wrapper = makeWrapper "ssh-wrapper" false ''
-              exec $LIBEXEC/nix-user-chroot "$NIX_STOREROOT/nix" $LIBEXEC/bash -c 'exec '"$SCRIPT_DIR"'/$SSH_ORIGINAL_COMMAND'
-            '';
-
-            nix-wrapper = makeWrapper "nix-wrapper" false ''
-              exec $LIBEXEC/nix-user-chroot "$NIX_STOREROOT/nix" $SCRIPT_DIR/nix \
-                --experimental-features 'nix-command flakes' \
-                --option sandbox false "$@"
-            '';
-
-            nix-store-wrapper = makeWrapper "nix-store-wrapper" false ''
-              exec $LIBEXEC/nix-user-chroot "$NIX_STOREROOT/nix" $SCRIPT_DIR/nix-store \
-                "$@"
-            '';
-
-            proot-wrapper = makeWrapper "proot-wrapper" true ''
-              ROOT="$1"
-              shift
-              exec $SCRIPT_DIR/proot -b "$ROOT":/nix/ "$@"
-            '';
-
-            bwrap-wrapper = makeWrapper "bwrap-wrapper" true ''
-              ROOT="$1"
-              shift
-              rootArgs=""
-              for path in /* ; do
-                if [ "$path" != "/nix" ] && [ "$path" != "/dev" ] ; then
-                  rootArgs="$rootArgs --bind $path $path"
-                fi
-              done
-              if [ ! -z ''${USER+x} ] ; then
-                rootArgs="$rootArgs --bind ${TMPDIR}/passwd /etc/passwd --bind ${TMPDIR}/group /etc/group"
-              fi
-              exec $SCRIPT_DIR/bwrap --bind "$ROOT" /nix/ $rootArgs --dev-bind /dev /dev "$@"
-            '';
-
-            proot = pkgs.fetchurl {
-              url = "https://github.com/proot-me/proot/releases/download/v5.2.0/proot-v5.2.0-x86_64-static";
-              sha256 = "sha256-6wZDs8SnfGsoe8Ev0WrXqUhs0vPE72Oh7oyn74pK4vA=";
-            };
-
-            bundler = what:
-              pkgs.runCommand "build-bundle" { } ''
-                ${arx'}/bin/arx tmpx --tmpdir '${TMPDIR}' ${tarball} // ${what} > $out
-                chmod 755 $out
-              '';
-
-            tarball =
-              pkgs.runCommand "slurm-nix.tar.bz2" { } ''
-                install -Dm 755 ${slurmNix}/bin/nix out/bin/nix
-                install -Dm 755 ${pkgs.pkgsStatic.bash}/bin/bash out/libexec/nix/bash
-                install -Dm 755 ${ssh-wrapper} out/bin/ssh-wrapper
-                install -Dm 755 ${nix-wrapper} out/bin/nix-wrapper
-                install -Dm 755 ${nix-store-wrapper} out/bin/nix-store-wrapper
-                install -Dm 755 ${chroot-wrapper} out/bin/chroot-wrapper
-                ln -s ../../bin/nix out/libexec/nix/build-remote
-
-                ${if useProot then ''
-                  install -Dm 755 ${proot} out/libexec/nix/proot
-                  install -Dm 755 ${proot-wrapper} out/libexec/nix/nix-user-chroot
-                '' else ''
-                  install -Dm 755 ${pkgs.pkgsStatic.bubblewrap}/bin/bwrap out/libexec/nix/bwrap
-                  install -Dm 755 ${bwrap-wrapper} out/libexec/nix/nix-user-chroot
-                ''}
-
-
-                for cmd in build channel collect-garbage copy-closure daemon env hash instantiate prefetch-url shell store ; do
-                  ln -s ./nix out/bin/nix-$cmd
-                done
-
-                tar -Jcvf $out -C ./out .
-              '';
-
-          in
-          {
-            slurm = tarball;
-            slurm-ssh-wrapper-bundle = bundler "ssh-wrapper";
-            slurm-nix-wrapper-bundle = bundler "nix-wrapper";
-            slurm-nix-store-wrapper-bundle = bundler "nix-store-wrapper";
-          };
-
-        defaultPackage =
-          pkgs.runCommand "build-package" { } ''
-            install -D ${patchedNix}/bin/nix $out/bin/nix
-            install -D ${pkgs.pkgsStatic.bash}/bin/bash $out/libexec/nix/bash
-            ln -s ../../bin/nix $out/libexec/nix/build-remote
-          '';
+  outputs = {
+    self,
+    nix,
+    nixpkgs,
+    flake-utils,
+    arxsrc,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      patchedNix = nix.packages."${system}".nix-static.overrideAttrs (_: {
+        patches = [./ca-cert-path.patch ./relocatable.patch ./cross-fingers.patch ./fixed-output.patch ./registration-time.patch];
+        doCheck = false;
       });
+    in {
+      packages = let
+        # Location of srun & sallocate binaries on the HPC system
+        SLURMPREFIX = "/usr/local/slurm/current/bin";
+
+        # Location of nix store
+        # Must be shared with build nodes
+        STOREROOT = "/vast/scratch/users/$USER";
+
+        # Location of temporary files (e.g., build directories)
+        # Must be shared with build nodes
+        TMPDIR = "${STOREROOT}/tmp";
+
+        # Use proot instead of bubblewrap
+        useProot = false;
+
+        SRUN = "${SLURMPREFIX}/srun";
+        SALLOC = "${SLURMPREFIX}/salloc";
+
+        makeWrapper = name: minimal: script:
+          pkgs.writeScript name ''
+            #!/bin/sh
+            set -e
+            set -o pipefail
+
+            function dirof {
+              SRC="$1"
+              BASE=''${SRC##*/}
+              DIR=''${SRC%"$BASE"}
+            }
+            dirof "$0"
+            SCRIPT_DIR="$( cd -- "$DIR" &> /dev/null && pwd -P)"
+            ${pkgs.lib.optionalString (!minimal) ''
+              dirof "$SCRIPT_DIR"
+              LIBEXEC="''${DIR}libexec/nix"
+              export TMPDIR="${TMPDIR}"
+              export NIX_STOREROOT="${STOREROOT}"
+              export NIX_SRUN="${SRUN}"
+              export NIX_SALLOC="${SALLOC}"
+              command -v mkdir &> /dev/null && mkdir -p "$TMPDIR"
+              command -v mkdir &> /dev/null && mkdir -p "$NIX_STOREROOT"/nix
+            ''}
+
+            # NSS wrapper remapping
+            if [ ! -z ''${USER+x} ] ; then
+              if [ ! -e ${TMPDIR}/passwd ] || [ ! -e ${TMPDIR}/group ] ; then
+                cat /etc/passwd > ${TMPDIR}/passwd
+                echo "$(id -un)::$(id -u):$(id -g)::$HOME:" >> ${TMPDIR}/passwd
+                cat /etc/group > ${TMPDIR}/group
+                echo "$(id -gn)::$(id -g):"  >> ${TMPDIR}/group
+              fi
+            fi
+
+            ${script}
+          '';
+
+        slurmNix = patchedNix.overrideAttrs (attrs: {
+          patches = attrs.patches ++ [./slurm-submit.patch];
+        });
+
+        arx' =
+          pkgs.haskellPackages.arx.overrideAttrs
+          (_: {
+            version = "git";
+            src = arxsrc;
+            preConfigure = ''
+              echo "git" > version
+            '';
+            patches = [./arx-cwd.patch];
+          });
+
+        chroot-wrapper = makeWrapper "chroot-wrapper" false ''
+          exec $LIBEXEC/nix-user-chroot "$NIX_STOREROOT/nix" "$@"
+        '';
+
+        ssh-wrapper = makeWrapper "ssh-wrapper" false ''
+          exec $LIBEXEC/nix-user-chroot "$NIX_STOREROOT/nix" $LIBEXEC/bash -c 'exec '"$SCRIPT_DIR"'/$SSH_ORIGINAL_COMMAND'
+        '';
+
+        nix-wrapper = makeWrapper "nix-wrapper" false ''
+          exec $LIBEXEC/nix-user-chroot "$NIX_STOREROOT/nix" $SCRIPT_DIR/nix \
+            --experimental-features 'nix-command flakes' \
+            --option sandbox false "$@"
+        '';
+
+        nix-store-wrapper = makeWrapper "nix-store-wrapper" false ''
+          exec $LIBEXEC/nix-user-chroot "$NIX_STOREROOT/nix" $SCRIPT_DIR/nix-store \
+            "$@"
+        '';
+
+        proot-wrapper = makeWrapper "proot-wrapper" true ''
+          ROOT="$1"
+          shift
+          exec $SCRIPT_DIR/proot -b "$ROOT":/nix/ "$@"
+        '';
+
+        bwrap-wrapper = makeWrapper "bwrap-wrapper" true ''
+          ROOT="$1"
+          shift
+          rootArgs=""
+          for path in /* ; do
+            if [ "$path" != "/nix" ] && [ "$path" != "/dev" ] ; then
+              rootArgs="$rootArgs --bind $path $path"
+            fi
+          done
+          if [ ! -z ''${USER+x} ] ; then
+            rootArgs="$rootArgs --bind ${TMPDIR}/passwd /etc/passwd --bind ${TMPDIR}/group /etc/group"
+          fi
+          exec $SCRIPT_DIR/bwrap --bind "$ROOT" /nix/ $rootArgs --dev-bind /dev /dev "$@"
+        '';
+
+        proot = pkgs.fetchurl {
+          url = "https://github.com/proot-me/proot/releases/download/v5.2.0/proot-v5.2.0-x86_64-static";
+          sha256 = "sha256-6wZDs8SnfGsoe8Ev0WrXqUhs0vPE72Oh7oyn74pK4vA=";
+        };
+
+        bundler = what:
+          pkgs.runCommand "build-bundle" {} ''
+            ${arx'}/bin/arx tmpx --tmpdir '${TMPDIR}' ${tarball} // ${what} > $out
+            chmod 755 $out
+          '';
+
+        tarball = pkgs.runCommand "slurm-nix.tar.bz2" {} ''
+          install -Dm 755 ${slurmNix}/bin/nix out/bin/nix
+          install -Dm 755 ${pkgs.pkgsStatic.bash}/bin/bash out/libexec/nix/bash
+          install -Dm 755 ${ssh-wrapper} out/bin/ssh-wrapper
+          install -Dm 755 ${nix-wrapper} out/bin/nix-wrapper
+          install -Dm 755 ${nix-store-wrapper} out/bin/nix-store-wrapper
+          install -Dm 755 ${chroot-wrapper} out/bin/chroot-wrapper
+          ln -s ../../bin/nix out/libexec/nix/build-remote
+
+          ${
+            if useProot
+            then ''
+              install -Dm 755 ${proot} out/libexec/nix/proot
+              install -Dm 755 ${proot-wrapper} out/libexec/nix/nix-user-chroot
+            ''
+            else ''
+              install -Dm 755 ${pkgs.pkgsStatic.bubblewrap}/bin/bwrap out/libexec/nix/bwrap
+              install -Dm 755 ${bwrap-wrapper} out/libexec/nix/nix-user-chroot
+            ''
+          }
+
+
+          for cmd in build channel collect-garbage copy-closure daemon env hash instantiate prefetch-url shell store ; do
+            ln -s ./nix out/bin/nix-$cmd
+          done
+
+          tar -Jcvf $out -C ./out .
+        '';
+
+        prune = pkgs.stdenv.mkDerivation {
+          name = "prune";
+          buildInputs = [(pkgs.haskellPackages.ghcWithPackages (p: with p; [sqlite-simple]))];
+          unpackPhase = ''
+            cp ${./prune.hs} ./prune.hs
+          '';
+          buildPhase = ''
+            ghc -O -o prune ${./prune.hs}
+          '';
+          installPhase = ''
+            install -Dm755 prune $out/bin/prune
+          '';
+        };
+      in {
+        inherit prune;
+        slurm = tarball;
+        slurm-ssh-wrapper-bundle = bundler "ssh-wrapper";
+        slurm-nix-wrapper-bundle = bundler "nix-wrapper";
+        slurm-nix-store-wrapper-bundle = bundler "nix-store-wrapper";
+      };
+
+      defaultPackage = pkgs.runCommand "build-package" {} ''
+        install -D ${patchedNix}/bin/nix $out/bin/nix
+        install -D ${pkgs.pkgsStatic.bash}/bin/bash $out/libexec/nix/bash
+        ln -s ../../bin/nix $out/libexec/nix/build-remote
+      '';
+    });
 }
---
+++ prune.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Control.Monad
+import qualified Data.Set                      as S
+import qualified Data.Text                     as T
+import qualified Data.Text.IO                  as T
+import           Data.Time.Clock.POSIX          ( getPOSIXTime )
+import           Database.SQLite.Simple  hiding ( close )
+import           System.Environment             ( getArgs )
+import           System.FilePath
+
+storeRoot = "/vast/scratch/users/bedo.j"
+
+newtype ID = ID Int deriving (Ord, Show, Eq)
+
+instance FromRow ID where
+  fromRow = ID <$> field
+
+instance ToRow ID where
+  toRow (ID id) = toRow $ Only id
+
+collect l r = pure $ S.insert r l
+
+closure :: Connection -> S.Set ID -> IO (S.Set ID)
+closure conn ids = do
+  ids' <- S.unions . S.insert ids . S.fromList <$> mapM q (S.toList ids)
+  if S.size ids' /= S.size ids then closure conn ids' else return ids
+ where
+  q :: ID -> IO (S.Set ID)
+  q id = fold conn
+              "select referrer from Refs where reference = ?"
+              id
+              S.empty
+              collect
+
+getId :: Connection -> FilePath -> IO ID
+getId conn path = head <$> queryNamed
+  conn
+  "select id from ValidPaths where path = :path"
+  [":path" := path]
+
+main = withConnection (storeRoot </> "/nix/var/nix/db/db.sqlite") $ \conn -> do
+  args <- getArgs
+  execute_ conn "pragma foreign_keys = off"
+
+  ps <- if length args > 0
+    then S.fromList <$> mapM (getId conn) args
+    else do
+      mark <- round <$> getPOSIXTime
+      fold conn
+           "select id from ValidPaths where registrationTime < ?"
+           (Only (mark - 60 * 60 * 24 * 13 :: Int))
+           S.empty
+           collect
+
+  ps' <- closure conn ps
+
+  putStrLn $ unwords ["deleting", show (S.size ps')]
+
+  withTransaction conn $ do
+    executeMany conn "delete from ValidPaths where id = ?" $ S.toList ps'
+    executeMany conn "delete from Refs where referrer = ?" $ S.toList ps'
+    executeMany conn "delete from Refs where reference = ?" $ S.toList ps'
+    executeMany conn "delete from DerivationOutputs where drv = ?"
+      $ S.toList ps'
